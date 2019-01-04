2738032a3f15c4320f8145894cf1280bb7accb5a
ShobhaJ <shobha.mce2003@gmail.com>
2019-01-04 16:04:35 +0530

    First commit

--- src/test/java/com/ibm/basetest/BaseTest.java
+++ src/test/java/com/ibm/basetest/BaseTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -19,7 +20,8 @@ public class BaseTest {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
 		System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new InternetExplorerDriver();
+		//WebDriver driver = new ChromeDriver();
 		WebDriverWait wait = new WebDriverWait(driver, 60); // 60 to 90
 
 		driver.manage().window().maximize();
